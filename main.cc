4a6ce3f3f72b342de2dc2014d1960f0c9f2a6086
hekciu <jakublasecki321@gmail.com>
2025-03-05 20:47:03 +0100

    call js from c++

--- main.cc
+++ main.cc
@@ -1,5 +1,19 @@
+#include <stdint.h>
+
+extern "C" void console_log(uint32_t value);
+extern uint8_t memory;
+
+
+static void print(uint32_t value) {
+    console_log(value);
+};
 
 
 extern "C" long add(long a, long b) {
     return a + b;
+};
+
+
+extern "C" void run() {
+    console_log(2137);
 };
